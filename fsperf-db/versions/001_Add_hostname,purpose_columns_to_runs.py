92a96f5001a3c423bc009d9faaf0f5fb0f77a147
Josef Bacik <josef@toxicpanda.com>
2021-03-16 14:05:41 -0400

    fsperf: use the hostname for the upgrade script
    Might as well use the actual default that we'll want.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fsperf-db/versions/001_Add_hostname,purpose_columns_to_runs.py
+++ fsperf-db/versions/001_Add_hostname,purpose_columns_to_runs.py
@@ -1,11 +1,11 @@
 from sqlalchemy import *
 from migrate import *
-
+import socket
 
 def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
     runs = Table('runs', meta, autoload=True)
-    hostnamec = Column('hostname', String, default="localhost")
+    hostnamec = Column('hostname', String, default=socket.gethostname())
     purposec = Column('purpose', String, default="continuous")
     hostnamec.create(runs)
     purposec.create(runs)
